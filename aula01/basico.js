293ccd1b26e4ed5393c46cfc1e63ef3ae92276e2
enriko lautenschlaeger <enrikolaute2008@gmail.com>
2026-03-19 22:25:02 -0300

    Aula sobre Vaiaveis

---
+++ aula01/basico.js
@@ -0,0 +1,16 @@
+//  javascript e uma linguagem fracamente tipada
+
+var VariavelVar = "ola"
+let VariavelLet = 'tudo bem?'
+const variavelconst = 13
+
+console.log("Ola mundo!")
+console.log("varivelVar") 
+console.log(variavelvar, VariavelLet) 
+console.log(variavelvar + variavelLet)
+console.log(variavelconst + variavelconst)
+variavelLet = 4
+console,log(VariavelLet)
+
+
+console.log(variavelconst) 
